59861e14634ecee2dcde3ea1d031ae9e09691326
sunyucc <799958317@qq.com>
2016-10-14 15:22:59 +0800

    实现闪屏功能

---
+++ app/src/main/java/cn/ucai/fulicenter/Activity/SlpashActivity.java
@@ -0,0 +1,36 @@
+package cn.ucai.fulicenter.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class SlpashActivity extends AppCompatActivity {
+private final long sleepTime  = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slpash2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                long costTime = System.currentTimeMillis()-startTime;
+                if(sleepTime-costTime>0){
+                    try {
+                        Thread.sleep(sleepTime-costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SlpashActivity.this,MainActivity.class));
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/cn/ucai/fulicenter/bean/AlbumsBean.java
@@ -0,0 +1,59 @@
+package cn.ucai.fulicenter.bean;
+
+/**
+ * Created by Administrator on 2016/10/13 0013.
+ */
+
+public class AlbumsBean {
+
+    private int pid;
+    private int imgId;
+    private String imgUrl;
+    private String thumbUrl;
+
+    public AlbumsBean() {
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getThumbUrl() {
+        return thumbUrl;
+    }
+
+    public void setThumbUrl(String thumbUrl) {
+        this.thumbUrl = thumbUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "AlbumsBean{" +
+                "pid=" + pid +
+                ", imgId=" + imgId +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", thumbUrl='" + thumbUrl + '\'' +
+                '}';
+    }
+}
+
