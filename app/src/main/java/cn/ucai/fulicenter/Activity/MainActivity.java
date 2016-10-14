8dc6fd47fd2d3750e580ce54cb93f56c21c27254
sunyucc <799958317@qq.com>
2016-10-14 17:20:17 +0800

    删除多余的包

--- app/src/main/java/cn/ucai/fulicenter/Activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/Activity/MainActivity.java
@@ -3,13 +3,9 @@ package cn.ucai.fulicenter.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RadioButton;
-
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.L;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
