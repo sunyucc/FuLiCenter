913db9a2b3aac1fb83522f89dfd510c152b65622
sunyucc <799958317@qq.com>
2016-10-19 00:53:47 +0800

    创建精品Fragment，用ListView实现布局

---
+++ app/src/main/java/cn/ucai/fulicenter/fragment/BoutiqueFragment.java
@@ -0,0 +1,15 @@
+package cn.ucai.fulicenter.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.fulicenter.R;
+
+public class BoutiqueFragment extends Fragment {
+
+}
